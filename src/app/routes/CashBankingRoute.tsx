f50dac301ca7a0ef361bdc3a1ceb36c5d79f0286
Juvialski <157385835+Juvialski@users.noreply.github.com>
2026-08-27 21:01:55 +0800

    feat: mount settlement allocation workflow in cash route

--- src/app/routes/CashBankingRoute.tsx
+++ src/app/routes/CashBankingRoute.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { CashBankingPage, type CashBankingPageProps } from "../../components/CashBankingPage.tsx";
+import { CashSettlementAllocationWorkspace } from "../../components/CashSettlementAllocationWorkspace.tsx";
 
 export type CashBankingRouteProps = CashBankingPageProps;
 
 export const CashBankingRoute: React.FC<CashBankingRouteProps> = (props) => {
-  return <CashBankingPage {...props} />;
+  return <div className="space-y-5">
+    <CashSettlementAllocationWorkspace
+      data={props.data}
+      candidates={props.reconciliationCandidates || []}
+      canReconcile={props.canReconcile}
+      onSaveMatch={props.onSaveMatch}
+    />
+    <CashBankingPage {...props} />
+  </div>;
 };
 
 export default CashBankingRoute;
